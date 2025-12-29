64943502cefdc25e4fffb302a02ef645d881ff01
asfTech <asfyar123@gmail.com>
2025-12-29 13:14:35 +0500

    Added Product Controller - API

---
+++ Inventory_API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("GetProductsAll")]
+        public IActionResult GetProductsAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetProductByProductID/{ProductID:int}")]
+        public IActionResult GetProductByProductID([FromRoute] int ProductID)
+        {
+            return Ok();
+        }
+    }
+}
--- Inventory_API/Dtos/ProductsDto.cs
+++ Inventory_API/Dtos/ProductsDto.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string SKU { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
         public bool IsActive { get; set; }
     }
 }
