7c46f4854f0398e8d7534a0654a6f063bac3252a
asfTech <asfyar123@gmail.com>
2026-01-02 00:32:58 +0500

    Added Dependencies Middleware

---
+++ Inventory_API/Middlewares/DependenciesMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Inventory_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_API.Middlewares
+{
+    public static class DependenciesMiddleware
+    {
+        public static IServiceCollection AddConnectionStrin(this IServiceCollection _Service,IConfiguration _config)
+        {
+            var ConnectionStr = _config.GetConnectionString("ConnectionStr");
+            _Service.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(ConnectionStr));
+
+            return _Service;
+        }
+
+    }
+}
--- Inventory_API/Program.cs
+++ Inventory_API/Program.cs
@@ -1,3 +1,5 @@
+using Inventory_API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependencies Middleware
+builder.Services.AddConnectionStrin(builder.Configuration);
 
 var app = builder.Build();
 
@@ -22,4 +27,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
